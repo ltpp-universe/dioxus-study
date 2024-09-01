5cb4199baa4de4c73c957626c8cf7be6c4f6647c
尤雨东 <root@ltpp.vip>
2024-09-01 22:37:42 +0800

    feat:init

---
+++ sh/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git remote add ltpp ssh://git@git.ltpp.vip:66/root/dioxus-study.git;
+git remote add github git@github.com:ltpp-universe/dioxus-study.git;
+git remote add origin git@github.com:ltpp-universe/dioxus-study.git;
