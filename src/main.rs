0ff9d73e411cc61ba5510e0c0ba788ceb1834898
尤雨东 <root@ltpp.vip>
2024-09-07 12:44:11 +0800

    feat: flex布局

--- src/main.rs
+++ src/main.rs
@@ -37,16 +37,27 @@ fn Home() -> Element {
     let mut count: Signal<i32> = use_signal(|| 0);
 
     rsx! {
-        Link {
-            to: Route::Blog {
-                id: count()
-            },
-            "Go to blog"
-        }
         div {
-            h1 { "High-Five counter: {count}" }
-            button { onclick: move |_| count += 1, "Up high!" }
-            button { onclick: move |_| count -= 1, "Down low!" }
+            // 新增的 fixed 布局背景元素
+            div {
+                style: "position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; background-color: lightblue; z-index: -10;",
+                h1 { "High-Five counter" }
+            },
+            // 使用 flex 布局的 div
+            div {
+                style: "display: flex; justify-content: space-around; align-items: center; height: 100px;",
+                h1 { "High-Five counter: {count}" }
+                button { onclick: move |_| count += 1, "Up high!" }
+                button { onclick: move |_| count -= 1, "Down low!" }
+            }
+
+            // 跳转链接
+            Link {
+                to: Route::Blog {
+                    id: count()
+                },
+                "Go to blog"
+            }
         }
     }
 }
